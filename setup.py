325ac7df273baad11fb714e68e69fb8bdf0ae021
pranavraj575 <40303088+pranavraj575@users.noreply.github.com>
2024-05-19 19:53:39 -0400

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='terminatorzero',
+    version='6.9.0',
+    packages=find_packages(),
+    install_requires=[
+                      ],
+    license='Liscence to Krill',
+)
